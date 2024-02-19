18f5b365ab8d913feeb726a532d91e257d16ee4d
antonbiluta <antonbiluta@gmail.com>
2024-02-19 11:33:52 +0300

    #5: Configure CORS

--- src/main/kotlin/ru/biluta/memes/service/config/OpenApiConfig.kt
+++ src/main/kotlin/ru/biluta/memes/service/config/OpenApiConfig.kt
@@ -1,5 +1,6 @@
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,9 +10,12 @@ class OpenApiConfig{
 
     @Bean
     fun customOpenAPI(): OpenAPI {
-        return OpenAPI().info(Info()
-                .title("MemesApp")
-                .description("API для работы с мемами")
+        return OpenAPI()
+                .info(Info()
+                        .title("MemesApp")
+                        .description("API для работы с мемами")
+                )
+                .servers(listOf(Server().url("/").description("Default Server URL"))
         )
     }
 
